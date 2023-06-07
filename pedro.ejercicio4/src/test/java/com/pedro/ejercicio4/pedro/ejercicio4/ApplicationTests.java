63dd7d154c6c2f3cbadb6132e09d31679db34fb1
pedrogard74 <135701485+pedrogard74@users.noreply.github.com>
2023-06-07 10:14:20 +0200

    Add files via upload

---
+++ pedro.ejercicio4/src/test/java/com/pedro/ejercicio4/pedro/ejercicio4/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.ejercicio4.pedro.ejercicio4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
